14defa29a76972b0a5edfd1024b36e928e813640
yyqyang <yyqyang59@hotmail.com>
2021-02-10 15:30:05 -0800

    add list view in the new activity

--- app/src/main/java/com/example/numad21sp_yuqingyang/MainActivity.java
+++ app/src/main/java/com/example/numad21sp_yuqingyang/MainActivity.java
@@ -14,7 +14,6 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
--- app/src/main/java/com/example/numad21sp_yuqingyang/MainActivity2.java
+++ app/src/main/java/com/example/numad21sp_yuqingyang/MainActivity2.java
@@ -3,12 +3,60 @@ package com.example.numad21sp_yuqingyang;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity2 extends AppCompatActivity {
+    ArrayList<String> listItems = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    private ListView myListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
+        FloatingActionButton fab = findViewById(R.id.fab_1);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addListItem();
+                Snackbar.make(view, "Item added to list",
+                        Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        myListView = findViewById(R.id.listView);
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+        myListView.setAdapter(adapter);
+
+    }
+
+    private void addListItem() {
+        SimpleDateFormat dateformat =
+                new SimpleDateFormat("HH:mm:ss MM/dd/yyyy",
+                        Locale.US);
+        listItems.add(dateformat.format(new Date()));
+        adapter.notifyDataSetChanged();
+    }
+
+
+
 }
